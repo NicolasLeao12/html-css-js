939abc475a72cc864729b1cdda009227b3c1323a
VictorCout0 <nleao213@gmail.com>
2025-10-25 15:32:43 -0300

    mudar formas

--- aula1/aula7/script.js
+++ aula1/aula7/script.js
@@ -1,18 +1,61 @@
-let cores = [
-"blue",
-"red",
-"grey",
-"green",
-"yellow",
-"brown",
-"purple",
-"orange",
-"pink",
+// let cores = [
+// "blue",
+// "red",
+// "grey",
+// "green",
+// "yellow",
+// "brown",
+// "purple",
+// "orange",
+// "pink"
+]
 
-
-
-
-
-
+let formas = [
+    "retangulo",
+    "quadrado",
+    "circulo",
+    "losango"
 
 ]
+
+
+
+
+
+
+
+// para "pegar" um elemnento do documento html:
+// document.getElementById(ID DO ELEMNTO)
+//document.querySelector("ID DO ELEMENTO")
+
+let divPrincipal = document.querySelector("#principal")
+let pNomeDaForma = document.querySelector("#nomedaforma")
+let index = 0
+
+
+function proximo(){
+
+    pNomeDaCor.innerText = cores[index]
+
+    if ( index > formas.length - 1 ) {
+        index = 0
+    }
+    // divPrincipal.style.backgroundColor = cores[index]
+    // index++
+    if ( formas[index] == "retangulo" ) {
+
+        divPrincipal.style.width = "400px"
+        divPrincipal.style.width = "200px"
+
+    }
+}
+
+function anterior(){
+        index--
+        if ( index < 0){
+            index = formas.length - 1
+        }
+    divPrincipal.style.backgroundColor = cores[index]
+    index--
+}
+ 
